48bd0a534d1640612155ac818533b9b07bd07c33
Arthur Neitzel <arthur.neitzel@basis.com.br>
2020-06-04 14:10:06 -0300

    Criado projeto inicial de backend

---
+++ backend/pvl/src/main/java/com/web/pvl/PvlApplication.java
@@ -0,0 +1,13 @@
+package com.web.pvl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PvlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PvlApplication.class, args);
+	}
+
+}
---
+++ backend/pvl/src/test/java/com/web/pvl/PvlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.pvl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PvlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
